72e1f371045d8d7fef454a01741dc3b795e369dd
Louis-Nicolas Longheval <louisnicolas.longheval@student.vinci.be>
2024-11-14 15:42:03 +0100

    feat(main): création de l'interface graphique dans le terminal
    Conducteur: Louni
    Navigateur: Moulay

---
+++ src/berlinClockTerminal.js
@@ -0,0 +1,74 @@
+const clc = require('cli-color');
+const getBerlinClockTime = require('./berlinClock');
+
+function displayBerlinClock(time) {
+  const [secondsLamp, fiveHoursRow, singleHoursRow, fiveMinutesRow, minutesRow] = getBerlinClockTime(time);
+
+  const displayLamp = (lamp) => {
+    if (lamp === 'R') {
+      return clc.bgRed('  ');
+    } else if (lamp === 'Y') {
+      return clc.bgYellow('  ');
+    } else {
+      return clc.bgBlackBright('  ');
+    }
+  };
+
+  console.clear();
+  console.log(clc.bold('Berlin Clock\n'));
+
+  // Lampe des secondes
+  console.log(`      ${displayLamp(secondsLamp)}     \n`);
+
+  // Lignes des heures
+  console.log(
+    '  ' +
+      fiveHoursRow
+        .split('')
+        .map(displayLamp)
+        .join(' ') +
+      '  \n'
+  );
+  console.log(
+    '  ' +
+      singleHoursRow
+        .split('')
+        .map(displayLamp)
+        .join(' ') +
+      '  \n'
+  );
+
+  // Lignes des minutes
+  console.log(
+    fiveMinutesRow
+      .split('')
+      .map((lamp, index) => {
+        if (lamp === 'R') {
+          return clc.bgRed('  ');
+        } else if (lamp === 'Y') {
+          return clc.bgYellow('  ');
+        } else {
+          return clc.bgBlackBright('  ');
+        }
+      })
+      .join(' ')
+  );
+  console.log(
+    '\n  ' +
+      minutesRow
+        .split('')
+        .map(displayLamp)
+        .join(' ') +
+      '  \n'
+  );
+  console.log(clc.bold(`Current Time : ${time}`));
+}
+const updateClock = () => {
+  const now = new Date();
+  const timeString = now.toTimeString().split(' ')[0];
+  displayBerlinClock(timeString);
+};
+
+setInterval(updateClock, 1000);
+
+updateClock();
